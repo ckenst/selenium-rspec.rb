778aff35b5f6f3a25f5eeff02a2f515e86a86ea6
Chris Kenst <ckenst@gmail.com>
2015-07-01 15:13:04 -0700

    Window resize and screen resolution for Sauce

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,19 +5,21 @@ require 'selenium-webdriver'
 RSpec.configure do |config|
 
 	config.before(:each) do
-		case ENV['host'] 
+		case ENV['host']
 		when 'saucelabs'
 			caps = Selenium::WebDriver::Remote::Capabilities.send(ENV['browser'])
       		caps.version = ENV['browser_version']
       		caps.platform = ENV['operating_system']
+					caps["screenResolution"] = ENV['resolution']
       		caps[:name] = example.metadata[:full_description]
-      
+
       		@driver = Selenium::WebDriver.for(
       			:remote,
       			url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
-      			desired_capabilities: caps) 
+      			desired_capabilities: caps)
       	else
-      		@driver = Selenium::WebDriver.for :firefox 
+      		@driver = Selenium::WebDriver.for :firefox
+					@driver.manage.window.resize_to(1280, 1024)
       	end
 	end
 
@@ -25,11 +27,11 @@ RSpec.configure do |config|
             if ENV['host'] == 'saucelabs'
                   if example.exception.nil?
                         SauceWhisk::Jobs.pass_job @driver.session_id
-                  else 
+                  else
                         SauceWhisk::Jobs.fail_job @driver.session_id
                   end
             end
-             
+
 		@driver.quit
 	end
 end
