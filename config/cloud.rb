90d97698feb032ef3971f9e8ff2c0acf09a08804
Chris Kenst <ckenst@gmail.com>
2016-06-09 08:39:35 -0700

    moved cloud config to it's own dir

--- config/cloud.rb
+++ config/cloud.rb
@@ -2,11 +2,11 @@
 
 require 'sauce_whisk'
 
-ENV['base_url']			||= 'http://the-internet.herokuapp.com'
-ENV['host']             = 'saucelabs'
+ENV['base_url']			    ||= 'http://the-internet.herokuapp.com'
+ENV['host']             ||= 'saucelabs'
 ENV['operating_system'] ||= 'Windows 8.1'
 ENV['browser']          ||= 'chrome'
 ENV['browser_version']  ||= '43'
-ENV['resolution'] ||= '1280x1024'
+ENV['resolution']       ||= '1280x1024'
 ENV['SAUCE_USERNAME']   ||= 'your-sauce-username'
 ENV['SAUCE_ACCESS_KEY'] ||= 'your-sauce-access-key'
