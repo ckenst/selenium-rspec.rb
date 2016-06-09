88a78fd15b5ff9d1620d0c59067d4458e7e7c9aa
Chris Kenst <ckenst@gmail.com>
2016-06-09 10:51:19 -0700

    two tests that need slightly less refactoring

--- spec/dropdown_spec.rb
+++ spec/dropdown_spec.rb
@@ -1,4 +1,4 @@
-#description goes here
+#Two slightly different ways to select from a dropdown list
 
 require_relative '../pages/dropdown'
 
@@ -13,12 +13,21 @@ describe 'Dropdown' do
     @driver.quit
   end
 
-  it 'selected' do
+  it 'selected one way' do #both of these need heavy refactoring
     dropdown_list = @driver.find_element(id: 'dropdown')
     options = dropdown_list.find_elements(tag_name: 'option')
     options.each { |option| option.click if option.text == 'Option 2' }
 
     selected_option = options.map { |option| option.text if option.selected? }.join
+    expect(selected_option).to eql 'Option 2'
+  end
+
+  it 'selected another way' do #both of these need heavy refactoring
+    dropdown_list = @driver.find_element(id: 'dropdown')
+    select_list = Selenium::WebDriver::Support::Select.new(dropdown_list)
+    select_list.select_by(:text, 'Option 2')
+
+    selected_option = select_list.selected_options[0].text
     expect(selected_option).to eql 'Option 2'
   end
 end
