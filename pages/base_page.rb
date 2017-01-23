664919acc4050204143085ecffc1c5d294ecace5
Chris Kenst <ckenst@gmail.com>
2017-01-22 16:57:58 -0800

    Adding common methods to base page object

--- pages/base_page.rb
+++ pages/base_page.rb
@@ -4,4 +4,16 @@ class BasePage
   def initialize(driver)
     @driver = driver
   end
+
+  def find(locator)
+    @driver.find_element locator
+  end
+
+  def type(text, locator)
+    find(locator).send_keys text
+  end
+
+  def click(locator)
+    find(locator).click
+  end
 end
