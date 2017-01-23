80e08f61191818635a4f3a9df205832e3d552d9e
Chris Kenst <ckenst@gmail.com>
2017-01-22 16:57:36 -0800

    Commented out sauce labs credentials

--- config/cloud.rb
+++ config/cloud.rb
@@ -1,5 +1,3 @@
-# filename: config_cloud.rb
-
 require 'sauce_whisk'
 
 ENV['base_url']			    ||= 'http://the-internet.herokuapp.com'
@@ -8,5 +6,6 @@ ENV['operating_system'] ||= 'Windows 8.1'
 ENV['browser']          ||= 'chrome'
 ENV['browser_version']  ||= '43'
 ENV['resolution']       ||= '1280x1024'
-ENV['SAUCE_USERNAME']   ||= 'your-sauce-username'
-ENV['SAUCE_ACCESS_KEY'] ||= 'your-sauce-access-key'
+# Can hard code here if necessary
+# ENV['SAUCE_USERNAME']   ||= 'your-sauce-username'
+# ENV['SAUCE_ACCESS_KEY'] ||= 'your-sauce-access-key'
