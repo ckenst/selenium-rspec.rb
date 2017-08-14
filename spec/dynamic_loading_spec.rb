134a040369ee89a8eaddc850f13e4a44f5f9d565
Chris Kenst <ckenst@gmail.com>
2017-08-13 23:43:33 -0700

    fixed two tests by removing spec_helper integration

--- spec/dynamic_loading_spec.rb
+++ spec/dynamic_loading_spec.rb
@@ -1,13 +1,18 @@
 # Showing how to do an implicit wait for dynamic loading page. Shows 2 tests
 
 require_relative '../pages/dynamic_loading'
-require_relative 'spec_helper'
+#require_relative 'spec_helper'
 
 describe 'Dynamic Loading' do
 
 	before(:each) do
+		@driver = Selenium::WebDriver.for :chrome
 		@dynamic_loading = DynamicLoading.new(@driver)
 	end
+
+	after(:each) do
+    @driver.quit
+  end
 
 	it 'Waited for Hidden Element' do
 		@dynamic_loading.visit_example 1
@@ -20,5 +25,4 @@ describe 'Dynamic Loading' do
 		@dynamic_loading.start
 		expect(@dynamic_loading.finish_text_present?).to eql true
 	end
-
 end
